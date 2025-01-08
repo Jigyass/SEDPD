0c262641fa1741a018904d16c61495fbdbf4e08a
Jigyass <jigyas@icloud.com>
2025-01-08 11:35:14 +0530

    sbatch dire

--- job.sh
+++ job.sh
@@ -18,7 +18,7 @@ source SHAP/bin/activate
 echo "Job ${SLURM_JOB_ID} running on ${HOSTNAME}"
 
 # Run your Python script
-python3 ResNet_BaseModel.py
+python3 Sbatch/ConvMixer_Cifar10.py
 
 # Optional: Deactivate virtual environment (cleanup)
 deactivate
