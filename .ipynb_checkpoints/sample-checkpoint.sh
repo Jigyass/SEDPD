56d102de1a09d19f1f497b7486095d013d3ca7cb
Jigyass <jigyas@icloud.com>
2025-01-10 13:01:28 +0000

    Eps 2-8

--- .ipynb_checkpoints/sample-checkpoint.sh
+++ .ipynb_checkpoints/sample-checkpoint.sh
@@ -5,21 +5,34 @@
 #SBATCH -c 16              # Request 4 CPU cores
 #SBATCH --mem=128GB        # Request 16GB memory
 #SBATCH -t 5-20:30:00      # Set a 30-minute time limit
-#SBATCH -J SHAP_Sample     # Job name
-#SBATCH -o LimeSm-%j.out   # Output file for logs
+#SBATCH -J Epsilon_Val     # Job name
+#SBATCH -o Slurm_Outputs/EpsilonV.out   # Output file for logs
 
 # Activate the SHAP virtual environment
 source SHAP/bin/activate
 
 # Run the Python scripts in parallel (hard-coded paths)
-python3 SamplingDicts_Lime/t2e1.py &
-python3 SamplingDicts_Lime/t2e2.py &
-python3 SamplingDicts_Lime/t2e3.py &
-python3 SamplingDicts_Lime/t2e4.py &
-python3 SamplingDicts_Lime/t2e5.py &
-python3 SamplingDicts_Lime/t2e6.py &
-python3 SamplingDicts_Lime/t2e7.py &
-python3 SamplingDicts_Lime/t2e8.py &
+python3 Sbatch/EpsConvCif/t2e3_ConvCif.py &
+python3 Sbatch/EpsConvCif/t2e4_ConvCif.py &
+python3 Sbatch/EpsConvCif/t2e5_ConvCif.py &
+python3 Sbatch/EpsConvCif/t2e6_ConvCif.py &
+python3 Sbatch/EpsConvCif/t2e7_ConvCif.py &
+python3 Sbatch/EpsConvCif/t2e8_ConvCif.py &
+python3 Sbatch/EpsResCif/t2e3_ResCif.py &
+python3 Sbatch/EpsResCif/t2e4_ResCif.py &
+python3 Sbatch/EpsResCif/t2e5_ResCif.py &
+python3 Sbatch/EpsResCif/t2e6_ResCif.py &
+python3 Sbatch/EpsResCif/t2e7_ResCif.py &
+python3 Sbatch/EpsResCif/t2e8_ResCif.py &
+python3 Sbatch/EpsResImg/t2e3_ResImg.py &
+python3 Sbatch/EpsResImg/t2e4_ResImg.py &
+python3 Sbatch/EpsResImg/t2e5_ResImg.py &
+python3 Sbatch/EpsResImg/t2e6_ResImg.py &
+python3 Sbatch/EpsResImg/t2e7_ResImg.py &
+python3 Sbatch/EpsResImg/t2e8_ResImg.py &
+
+
+
 
 # Wait for all scripts to finish
 wait
