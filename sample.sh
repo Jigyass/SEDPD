1052f68c3b7bc2e11df6badf1a131617c33199b8
Jigyass <jigyas@icloud.com>
2025-01-15 12:33:23 +0530

    Defense datasets

--- sample.sh
+++ sample.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 
 #SBATCH -p gpu
-#SBATCH --gres=gpu:a100:1  # Request 1 A100 GPU
-#SBATCH -c 48              # Request 4 CPU cores
-#SBATCH --mem=500GB        # Request 16GB memory
-#SBATCH -t 10-20:30:00     # Set a 30-minute time limit
-#SBATCH -J EpsOptimizd     # Job name
-#SBATCH -o Slurm_Outputs/EpsilonV_Opt.out   # Output file for logs
+#SBATCH --gres=gpu:p100:1                   # Request 1 A100 GPU
+#SBATCH -c 24                               # Request 4 CPU cores
+#SBATCH --mem=100GB                         # Request 16GB memory
+#SBATCH -t 10-20:30:00                      # Set a 30-minute time limit
+#SBATCH -J DefenseData                      # Job name
+#SBATCH -o Slurm_Outputs/ResCifDefData.out  # Output file for logs
 
 # Activate the SHAP virtual environment
 source SHAP/bin/activate
 
 # Run the Python scripts in parallel (hard-coded paths)
-python3 Sbatch/EpsConvCif/t2e3_ConvCif.py &
-python3 Sbatch/EpsConvCif/t2e4_ConvCif.py &
-python3 Sbatch/EpsConvCif/t2e5_ConvCif.py &
-python3 Sbatch/EpsConvCif/t2e6_ConvCif.py &
-python3 Sbatch/EpsConvCif/t2e7_ConvCif.py &
-python3 Sbatch/EpsConvCif/t2e8_ConvCif.py &
-python3 Sbatch/EpsResCif/t2e3_ResCif.py &
-python3 Sbatch/EpsResCif/t2e4_ResCif.py &
-python3 Sbatch/EpsResCif/t2e5_ResCif.py &
-python3 Sbatch/EpsResCif/t2e6_ResCif.py &
-python3 Sbatch/EpsResCif/t2e7_ResCif.py &
-python3 Sbatch/EpsResCif/t2e8_ResCif.py &
-
-
+python3 Sbatch/Defense/ResCif_e3.py &
+python3 Sbatch/Defense/ResCif_e4.py &
+python3 Sbatch/Defense/ResCif_e5.py &
+python3 Sbatch/Defense/ResCif_e6.py &
+python3 Sbatch/Defense/ResCif_e7.py &
+python3 Sbatch/Defense/ResCif_e8.py &
 
 
 # Wait for all scripts to finish
