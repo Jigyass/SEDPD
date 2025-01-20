0c5e2c5558c7a364387a951bc225a476da68dfaf
Jigyass <jigyas@icloud.com>
2025-01-20 12:33:38 -0600

    sbatch script

--- job.sh
+++ job.sh
@@ -2,11 +2,11 @@
 
 #SBATCH -p gpu                             # Specify the GPU partition
 #SBATCH --gres=gpu:a100:1                  # Request 1 A100 GPU
-#SBATCH -c 16                              # Request 16 CPU cores
-#SBATCH --mem=80GB                         # Request 80GB memory
+#SBATCH -c 96                              # Request 16 CPU cores
+#SBATCH --mem=180GB                        # Request 80GB memory
 #SBATCH -t 5-23:20:00                      # 5 hour time limit
-#SBATCH -J ResCifAtLim                     # Name of the job
-#SBATCH -o Slurm_Outputs/ResCifAtLime.out. # Save output to slurm-<job_id>.out
+#SBATCH -J ConvImgDefD                     # Name of the job
+#SBATCH -o Slurm_Outputs/ConvImgDataDef.out# Save output to slurm-<job_id>.out
 
 # Load modules (if needed) - Uncomment and customize as required
 # module load cuda/11.3
@@ -18,7 +18,7 @@ source SHAP/bin/activate
 echo "Job ${SLURM_JOB_ID} running on ${HOSTNAME}"
 
 # Run your Python script
-python3 Sbatch/ResLimeAtCif.py
+python3 Sbatch/DefDataConv.py
 
 # Optional: Deactivate virtual environment (cleanup)
 deactivate
